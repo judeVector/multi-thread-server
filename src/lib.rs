5d0be186661116b5480ce02a23b0b81d7dd9b9d2
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2025-08-10 01:19:25 +0100

    Update README

--- src/lib.rs
+++ src/lib.rs
@@ -86,6 +86,20 @@ impl Worker {
             }
         });
 
+        //     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        //     let thread = thread::spawn(move || {
+        //         loop {
+        //             let job = receiver.lock().unwrap().recv().unwrap();
+
+        //             println!("Worker {id} got a job; executing.");
+
+        //             job();
+        //         }
+        //     });
+
+        //     Worker { id, thread }
+        // }
+
         Worker {
             id,
             thread: Some(thread),
